38cf0ba76ac21a972c3b3ba4348890b74fb63ce3
Goose39 <Goose39.Dev@gmail.com>
2019-10-28 12:45:39 -0400

    js test

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function handleClick() {
+  $("form").submit(event, function() {
+  event.preventDefault();
+  let breed = $(".breed").val();
+  fetch (`https://dog.ceo/api/breed/${breed}/images/random/`)
+  .then(response => response.json())
+  .then(responseJson => {
+          console.log(`${responseJson.message}`)
+          displayImages(responseJson.message);
+          })
+  .catch(error => displayError(error.message))
+})
+}
+
+function displayImages(arr) {
+  let displayArea = $('.display-results');
+
+  displayArea.removeClass("hidden");
+  displayArea.html(`<h2>Your Dogs</h2>
+                    <img src="${img}" class="dog-img">`);
+}
+
+function displayError(errorMsg) {
+  displayArea.removeClass("hidden");
+  displayArea.html(`<h2>An error occured</h2>
+                    <p>${errorMsg}</p>`);
+}
+
+function printToConsole(msg) {
+  console.log(`${msg}`);
+}
+
+$(handleClick);
